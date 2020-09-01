136b411683b98e8ec58cd933218537c4b6fe1709
Jorge <jorl.villafuerte@gmail.com>
2020-09-01 19:47:48 -0300

    commit

--- src/main/java/ar/com/trazabilidad/util/Constants.java
+++ src/main/java/ar/com/trazabilidad/util/Constants.java
@@ -2,13 +2,11 @@ package ar.com.trazabilidad.util;
 
 public class Constants {
     
-    public static final String LOGIN_URL = "/login";
-    public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
-    public static final String TOKEN_BEARER_PREFIX = "Bearer ";
-
-	// JWT
+    //public static final String LOGIN_URL = "/login";
+    //public static final String HEADER_AUTHORIZACION_KEY = "Authorization";
+    //public static final String TOKEN_BEARER_PREFIX = "Bearer ";
 
     public static final String ISSUER_INFO = "https://www.trazabilidad.com/";
     public static final String SECRET_KEY = "trazabilidad";
-    public static final long TOKEN_EXPIRATION_TIME = 900000; // 30min
+    public static final long TOKEN_EXPIRATION_TIME = 900000; 
 }
